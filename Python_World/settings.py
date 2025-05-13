accf4eefd085ba86ae2b1b4c39a96b5e7cfa978f
Tirth Gajera <136673925+TIrth999999@users.noreply.github.com>
2025-05-13 10:35:03 +0530

    Update settings.py

--- Python_World/settings.py
+++ Python_World/settings.py
@@ -77,13 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Python_World.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
